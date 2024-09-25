f18d4ccdcc612e7c9886e23ce700adf3d72ca9c5
EragonKing2020 <Eragonking2020@gmail.com>
2024-09-25 11:28:40 +0200

    Time (showtime) service added

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -14,6 +14,17 @@ with open('{}/databases/times.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
 
+@app.route("/showtimes", methods=['GET'])
+def get_schedule():
+   return make_response(jsonify(schedule), 200)
+
+@app.route("/showmovies/<date>", methods=['GET'])
+def get_movies_bydate(date):
+   for day in schedule:
+      if day['date'] == date:
+         return make_response(day,200)
+   return make_response("Date not found", 400)
+
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
    app.run(host=HOST, port=PORT)

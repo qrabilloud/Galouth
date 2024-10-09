c84a5283ffc3e93eab81fb23af5bb614314606eb
EragonKing2020 <Eragonking2020@gmail.com>
2024-10-09 09:11:00 +0200

    Add deletion booking

--- booking/booking.py
+++ booking/booking.py
@@ -52,6 +52,40 @@ def add_booking_byuser(userid):
    write(bookings)
    return make_response(jsonify(booking), 200)
 
+def removeObjFromListAttr(list, val, attrVal = None):
+   print(list)
+   print(val)
+   indexs = []
+   i = 0
+   for elem in list:
+      if attrVal:
+         if elem[attrVal] == val:
+            indexs.append(i)
+      else:
+         if elem == val:
+            indexs.append(i)
+      i += 1
+   for iIndex in range(len(indexs) - 1, -1, -1):
+      del list[indexs[iIndex]]
+   if not list: return False
+   return True
+
+@app.route("/bookings/<userid>", methods=['DELETE'])
+def delete_booking_ofuser(userid):
+   req = request.get_json()
+   plannedMovies = requests.get("http://127.0.0.1:3202/showmovies/" + req['date']).json()['movies']
+   if not req['movie'] in plannedMovies:
+      return make_response("Movie not planned on this date", 409)
+   booking = getObjFromListAttr(bookings, 'userid', userid)
+   date = getObjFromListAttr(booking['dates'], 'date', req['date']) if booking else None
+   if not (date and req['movie'] in date['movies']):
+      return make_response("Movie not booked on this day", 409)
+   if not removeObjFromListAttr(date['movies'], req['movie']):
+      if not removeObjFromListAttr(booking['dates'], req['date'], 'date'):
+         removeObjFromListAttr(bookings, userid, 'userid')
+   write(bookings)
+   return make_response(jsonify(getObjFromListAttr(bookings, 'userid', userid)), 200)
+
 def write(bookings):
     with open('{}/databases/bookings.json'.format("."), 'w') as f:
         json.dump({"bookings" : bookings}, f)
